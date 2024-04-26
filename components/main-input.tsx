bc7df72b5110eaf076eb34073f56fd24398b5712
dmiante <dmi4ndev@gmail.com>
2024-04-26 18:41:55 -0400

    add api to create short link and fetching api

--- components/main-input.tsx
+++ components/main-input.tsx
@@ -2,48 +2,63 @@
 
 import { useForm } from "react-hook-form"
 import { Button } from "./ui/button"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./ui/form"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form"
 import { Input } from "./ui/input"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { z } from "zod"
+import { toast } from "sonner"
+
 
 const formSchema = z.object({
-  link: z.string().url({ message: 'Invalid url' })
+  url: z.string().url({ message: 'Invalid url' })
 })
 
 export default function MainInput() {
-      // 1. Define your form.
-      const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-          link: "",
-        },
-      })
+
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      url: ""
+    },
+  }) 
      
-      // 2. Define a submit handler.
-      function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
-      }
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      const res = await fetch('http://localhost:3000/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(values)
+      })
+      const data = await res.json()
+      console.log(data)
+      toast.success('Link created successfully!')
+    } catch (error) {
+      toast.error('An unexpected error has ocurred. Please try again later.')
+    }
+  }
   return (
-    <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)}
-    className="space-y-5 w-full">
-      <FormField
-        control={form.control}
-        name="link"
-        render={({ field }) => (
-          <FormItem className="w-full">
-            <FormControl>
-              <Input placeholder="Paste URL here" {...field}/>
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <Button type="submit" className="w-full text-lg font-bold">Shorten</Button>
-    </form>
-  </Form>
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-5 w-full">
+          <FormField
+            control={form.control}
+            name="url"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input placeholder="Paste URL here" {...field}/>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full text-lg font-bold">Shorten</Button>
+        </form>
+      </Form>
+  </>
   )
 }
