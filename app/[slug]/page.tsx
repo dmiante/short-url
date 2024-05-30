ded0bb2599f89735f3ad5da6c87851695d52e0ad
dmiante <dmi4ndev@gmail.com>
2024-05-30 03:10:37 -0400

    add: route to short url

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import { getSlug } from '@/server/data/links'
+import { notFound, permanentRedirect } from 'next/navigation'
+
+export default async function SlugPage({ params }: { params: { slug: string } }) {
+  const { slug } = params
+
+  const slugUrl = await getSlug(slug)
+  if(!slugUrl){
+    notFound()
+  }
+
+  return permanentRedirect(slugUrl.url)
+}
